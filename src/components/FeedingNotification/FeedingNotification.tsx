00511ce231a7117aad555e48b5e7d461c932ebb5
Marcus Reineck <100363230+MarcusRei@users.noreply.github.com>
2023-05-07 22:25:30 +0200

    (fix) bug fixed when all animals fed

--- src/components/FeedingNotification/FeedingNotification.tsx
+++ src/components/FeedingNotification/FeedingNotification.tsx
@@ -5,46 +5,56 @@ import "./FeedingNotification.css";
 import { DateTime, Interval } from "luxon";
 
 export const FeedingNotification = () => {
-  const [animals, setAnimals] = useState<IAnimal[]>(getFromLS("animals"));
+  const [animals, setAnimals] = useState<IAnimal[]>(getFromLS("animals") || []);
 
   const [hungryAnimals, setHungryAnimals] = useState<IAnimal[]>([]);
 
-  if (hungryAnimals.length === 0) {
+  /* if (hungryAnimals.length === 0) {
     checkHunger();
-  }
+  } */
 
   setInterval(() => {
     checkHunger();
   }, 5000);
 
   function checkHunger() {
-    const filterAnimals = animals.filter((animal) => {
+    const filteredAnimals = animals.filter((animal) => {
       const lastFeeding = DateTime.fromISO(animal.lastFed);
 
       const currentTime = DateTime.now();
       const interval = Interval.fromDateTimes(lastFeeding, currentTime);
       return interval.length("hours") > 3;
     });
-    setHungryAnimals(filterAnimals);
+    setHungryAnimals(filteredAnimals);
   }
 
-  return (
-    <div className="notification__container--outer">
-      {hungryAnimals.map((animal) => {
-        const altText = "Bild på " + animal.name;
-        return (
-          <div key={animal.id} className="notification__container--inner">
-            <img
-              className="notification__img"
-              src={animal.imageUrl}
-              alt={altText}
-            />
-            <p className="notification__text">
-              {animal.name} behöver bli matad!
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
+  if (hungryAnimals.length > 0) {
+    return (
+      <div className="notification__container--outer">
+        {hungryAnimals.map((animal) => {
+          const altText = "Bild på " + animal.name;
+          return (
+            <div key={animal.id} className="notification__container--inner">
+              <img
+                className="notification__img"
+                src={animal.imageUrl}
+                alt={altText}
+              />
+              <p className="notification__text">
+                {animal.name} behöver bli matad!
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return (
+      <div className="notification__container--outer">
+        <h2 className="notification__fed--message">
+          Alla djur är mätta och belåtna!
+        </h2>
+      </div>
+    );
+  }
 };
